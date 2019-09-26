6937510d64e1db17fc94fabc7443187efac58dc9
lcyzl <1321023225@qq.com>
2019-09-26 16:57:22 +0800

    完成编辑更新å·功能

--- src/main/java/com/cyz/community/community/mapper/UserMapper.java
+++ src/main/java/com/cyz/community/community/mapper/UserMapper.java
@@ -13,6 +13,6 @@ public interface UserMapper {
     User findById(@Param("id")Integer id);
     @Select("select * from user where account_id = #{accountId}")
     User findByAccountId(@Param("accountId")String accountId);
-    @Update("update question set name = #{name},token = #{token},gmt_modified = #{gmtModified},avatar_url = #{avatarUrl} where id = #{id}" )
+    @Update("update user set name = #{name},token = #{token},gmt_modified = #{gmtModified},avatar_url = #{avatarUrl} where id = #{id}" )
     void update(User dbUser);
 }

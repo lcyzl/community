a43138c226c63874f2885a0af0f3829f5ba727fe
lcyzl <1321023225@qq.com>
2019-09-30 15:04:17 +0800

    完成登录不刷新页面功能

--- src/main/java/com/cyz/community/community/service/CommentService.java
+++ src/main/java/com/cyz/community/community/service/CommentService.java
@@ -10,6 +10,7 @@ import com.cyz.community.community.model.Comment;
 import com.cyz.community.community.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CommentService {
@@ -21,6 +22,8 @@ public class CommentService {
 
     @Autowired
     private QuestionExtMapper questionExtMapper;
+
+    @Transactional
     public void insert(Comment comment) {
         if(comment.getParentId() == null || comment.getParentId() == 0){
             throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
---
+++ src/main/resources/static/js/community.js
@@ -0,0 +1,34 @@
+function post() {
+    var questionId = $("#question_id").val();
+    var content = $("#comment_content").val();
+    $.ajax({
+        type: "POST",
+        url: "/comment",
+        contentType:"application/json",
+        data: JSON.stringify({
+            "parentId":questionId,
+            "content":content,
+            "type":1
+        }),
+        success: function (response) {
+            if(response.code == 200){
+                $("#comment_section").hide();
+            }else{
+                if (response.code == 2003){
+                   var isAcceptes =  confirm(response.message);
+                   if(isAcceptes){
+                       window.open("https://github.com/login/oauth/authorize?client_id=77c8b76f787fe9ec4ab7&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                       window.localStorage.setItem("closable",true);
+                   }
+                }else {
+
+                    alert(response.message);
+                }
+            }
+        console.log(response)
+    },
+        dataType: "json"
+    });
+    console.log(questionId);
+    console.log(content);
+}
